dd913a48f62cc839cbdde560e35b92024988cd1f
Dziaba <wyrwisz.hubert@gmail.com>
2024-06-01 16:16:25 +0200

    zad2

--- main/src/main/java/pplab7/Main.java
+++ main/src/main/java/pplab7/Main.java
@@ -1,30 +1,27 @@
 package pplab7;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import java.io.File;
-
 public class Main extends Application {
-
     private TextField directoryPathField;
     private TextField searchField;
-
+    private TextArea resultArea;
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("File Browser and Search");
-
         directoryPathField = new TextField();
         directoryPathField.setPromptText("Enter directory path");
-
         searchField = new TextField();
+        resultArea = new TextArea();
+        resultArea.setPrefHeight(400);
         searchField.setPromptText("Enter search phrase");
-
         Button browseButton = new Button("Browse");
         browseButton.setOnAction(new javafx.event.EventHandler<javafx.event.ActionEvent>() {
             @Override
@@ -32,17 +29,13 @@ public class Main extends Application {
                 browseDirectory();
             }
         });
-
         Button searchButton = new Button("Search");
-
         HBox hBox = new HBox(10, directoryPathField, browseButton);
-        VBox vBox = new VBox(10, hBox, searchField, searchButton);
-
+        VBox vBox = new VBox(10, hBox, searchField, searchButton, resultArea);
         Scene scene = new Scene(vBox, 600, 200);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
     private void browseDirectory() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         File selectedDirectory = directoryChooser.showDialog(null);
@@ -50,7 +43,6 @@ public class Main extends Application {
             directoryPathField.setText(selectedDirectory.getAbsolutePath());
         }
     }
-
     public static void main(String[] args) {
         launch(args);
     }
